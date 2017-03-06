9c9d912b7db9b5145d62f0952a0e068f2de461f9
Tim Knudsen <TimTheSmartCoder@outlook.dk>
2017-03-06 12:00:33 +0100

    Added circle component.

--- FriendListApp/FriendListApp/Backend/Enities/Friend.cs
+++ FriendListApp/FriendListApp/Backend/Enities/Friend.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace FriendListApp.Backend.Enities
 {
-    class Friend
+    public class Friend
     {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public ImageSource Image { get; set; }
     }
 }
--- FriendListApp/FriendListApp/Pages/MainPage.xaml.cs
+++ FriendListApp/FriendListApp/Pages/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FriendListApp.Backend.Enities;
 using Xamarin.Forms;
 
 namespace FriendListApp
@@ -12,6 +13,17 @@ namespace FriendListApp
         public MainPage()
         {
             InitializeComponent();
+            this.FriendList.ItemsSource = new List<Friend>()
+            {
+                new Friend()
+                {
+                    Name = "Test",
+                    Location = "Test",
+                    Image =
+                        ImageSource.FromUri(
+                            new Uri("https://blog.xamarin.com/wp-content/uploads/2014/12/CircleImages-1024x611.png"))
+                }
+            };
         }
     }
 }
